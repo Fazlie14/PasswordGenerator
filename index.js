9de3724efd5b3643158671b3fd96fa03f60d20bb
Fazlie <93725034+Fazlie14@users.noreply.github.com>
2025-10-20 03:17:13 +0800

    update javascript function

--- index.js
+++ index.js
@@ -1,23 +1,114 @@
-let randomArr = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","Q","W","E","R","T","Y","U","I",'O',"P,","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","!","@","#","$","%","^","&","*","(",")","<",">","?","/",""]
+let randomArr = [
+  "q",
+  "w",
+  "e",
+  "r",
+  "t",
+  "y",
+  "u",
+  "i",
+  "o",
+  "p",
+  "a",
+  "s",
+  "d",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  "z",
+  "x",
+  "c",
+  "v",
+  "b",
+  "n",
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P,",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "<",
+  ">",
+  "?",
+  "/",
+  "",
+];
+
+let displayOneEl = document.getElementById("displayOne-El");
+let displayTwoEl = document.getElementById("displayTwo-El");
+
+let generateBtn = document.getElementById("generate-Btn");
+let inputEl = document.getElementById("input-El")
+let errorMessage = document.getElementById("error-El");
 
 
-let displayOneEl = document.getElementById("displayOne-El")
-let displayTwoEl = document.getElementById("displayTwo-El")
 
-let generateBtn = document.getElementById("generate-Btn")
-
-generateBtn.addEventListener("click",function(){
+generateBtn.addEventListener("click", function () {
   
-    displayOneEl.textContent = ""
-    displayTwoEl.textContent = ""
-    for(let i = 0; i < 15; i++){
-      let randomIndexOne = Math.floor(Math.random() * randomArr.length)
-      let randomIndexTwo = Math.floor(Math.random() * randomArr.length)
-      displayOneEl.textContent += randomArr[randomIndexOne]
-      displayTwoEl.textContent +=            randomArr[randomIndexTwo]
-    }
-})
+ 
+  let inputValue = parseInt(inputEl.value);
 
-for(let i = 0; i < Array.length; i++){
-  console.log(Array[i])
-}
+  if(inputEl.value === 0 || inputEl.value === "" || isNaN(inputValue)){
+    inputEl.style.border = "1px solid red";
+    errorMessage.textContent = "⚠️ Please Enter password Length!";
+    
+  }else{
+    inputEl.style.border = "";
+    errorMessage.textContent = "";
+    displayOneEl.textContent = "";
+    displayTwoEl.textContent = "";
+  }
+
+  
+  
+  
+  for (let i = 0; i < inputValue; i++) {
+    let randomIndexOne = Math.floor(Math.random() * randomArr.length);
+    let randomIndexTwo = Math.floor(Math.random() * randomArr.length);
+    displayOneEl.textContent += randomArr[randomIndexOne];
+    displayTwoEl.textContent += randomArr[randomIndexTwo];
+  }
+  
+  
+ 
+  inputEl.value = ""
+
+  
+
+});
+
+
+
